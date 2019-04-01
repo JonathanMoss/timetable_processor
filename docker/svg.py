e3480d53f705998c9235278c9588b8bcd6d3e559
joth.moss@googlemail.com <joth.moss@googlemail.com>
2019-04-01 21:24:50 +0100

    Complete re-write of association engine

--- docker/svg.py
+++ docker/svg.py
@@ -19,7 +19,7 @@ class SVGObject:
 		x = json.loads(json_string)
 		return [datetime.strptime(x['start_time'], '%H:%M'), datetime.strptime(x['end_time'], '%H:%M')]
 
-	def draw_platform_index(self):
+	def draw_docker_background(self):
 
 		# Calculate row height
 		sep_line_height = 2  # Pixels height
@@ -30,39 +30,37 @@ class SVGObject:
 		y = 0
 		alt=True
 
-		blue_row = self.dwg.rect(id='blue_row', size=(self.svg_width, row_height), fill='blue', opacity='0.179')
-		green_row = self.dwg.rect(id='green_row', size=(self.svg_width, row_height), fill='green', opacity='0.179')
-		self.dwg.defs.add(blue_row)
-		self.dwg.defs.add(green_row)
+		blue_row = self.main_dwg.rect(id='blue_row', size=(self.main_svg_width, row_height), fill='blue', opacity='0.179')
+		green_row = self.main_dwg.rect(id='green_row', size=(self.main_svg_width, row_height), fill='green', opacity='0.179')
+		self.main_dwg.defs.add(blue_row)
+		self.main_dwg.defs.add(green_row)
 		
 		for platform in self.platforms:
 			
 			#Add background rectangle
 			if alt:
-				#self.dwg.add(self.dwg.rect((0, y), (self.svg_width, row_height), fill='blue', opacity='0.179'))
-				self.dwg.add(self.dwg.use(blue_row, insert=(0, y)))
+				#self.main_dwg.add(self.main_dwg.rect((0, y), (self.svg_width, row_height), fill='blue', opacity='0.179'))
+				self.main_dwg.add(self.main_dwg.use(blue_row, insert=(0, y)))
 				alt = False
 			else:
-				#self.dwg.add(self.dwg.rect((0, y), (self.svg_width, row_height), fill='green', opacity='0.179'))
-				self.dwg.add(self.dwg.use(green_row, insert=(0, y)))
+				#self.main_dwg.add(self.main_dwg.rect((0, y), (self.svg_width, row_height), fill='green', opacity='0.179'))
+				self.main_dwg.add(self.main_dwg.use(green_row, insert=(0, y)))
 				alt = True
 
 			y += sep_line_height + row_height
 
-			# Add text label
-			self.dwg.add(self.dwg.text(platform, insert=(10, y - 12), stroke='none', fill='black', font_size='20px', font_weight="bold", font_family="Arial"))
-			
-			# Add seperator line	
-			self.dwg.add(self.dwg.line(start=(0,y-1), end=(self.svg_width, y-1), stroke_width=2, stroke='black'))
+			# # Add seperator line
+			# if total_sep > 0:  # Ensures we only draw the right amount of seperators
+			# 	self.main_dwg.add(self.main_dwg.line(start=(0,y-1), end=(self.main_svg_width, y-1), stroke_width=2, stroke='black'))
+			# 	total_sep -= 1
 
-		# Add number column
-		self.dwg.add(self.dwg.rect((0,0), (42, 500), fill='blue', opacity='0.179'))
+
 
 	def draw_time_line(self, start_time, end_time):
 		
 		# Draw time line
-		self.dwg.add(self.dwg.line(start=(43, 520), end=(self.svg_width, 520), stroke_width=2, stroke='black'))
-		self.dwg.add(self.dwg.rect((36, 508), (self.svg_width - 26, 32), rx='5', ry='5', fill='yellow', opacity='0.179', stroke='black'))
+		self.main_dwg.add(self.main_dwg.line(start=(43, 520), end=(self.svg_width, 520), stroke_width=2, stroke='black'))
+		self.main_dwg.add(self.main_dwg.rect((36, 508), (self.svg_width - 26, 32), rx='5', ry='5', fill='yellow', opacity='0.179', stroke='black'))
 		time_range = DateTimeRange(start_time, end_time)
 		tm_between = end_time - start_time	
 		range_time = int(tm_between.total_seconds() / 60)  # Minutes between start and end time...
@@ -73,8 +71,8 @@ class SVGObject:
 		
 		# hour = int(start_time.hour)
 		# for y in range(43, int(self.svg_width), int(hour_ticks)):
-		# 	#self.dwg.add(self.dwg.line(start=(y, 510), end=(y, 530), stroke_width=2, stroke='red'))
-		# 	self.dwg.add(self.dwg.text(hour, insert=(y-5, 538), fill='black', font_size='10px', font_weight='bold', font_family='Arial'))
+		# 	#self.main_dwg.add(self.main_dwg.line(start=(y, 510), end=(y, 530), stroke_width=2, stroke='red'))
+		# 	self.main_dwg.add(self.main_dwg.text(hour, insert=(y-5, 538), fill='black', font_size='10px', font_weight='bold', font_family='Arial'))
 		# 	hour += 1
 
 		y = 43
@@ -86,14 +84,14 @@ class SVGObject:
 					hr = '0{}'.format(str(val.hour))
 				else:
 					hr = str(val.hour)
-				self.dwg.add(self.dwg.line(start=(y, 510), end=(y, 530), stroke_width=2, stroke='red'))
-				self.dwg.add(self.dwg.text(hr, insert=(y-5, 538), fill='black', font_size='10px', font_weight='bold', font_family='Arial'))
+				self.main_dwg.add(self.main_dwg.line(start=(y, 510), end=(y, 530), stroke_width=2, stroke='red'))
+				self.main_dwg.add(self.main_dwg.text(hr, insert=(y-5, 538), fill='black', font_size='10px', font_weight='bold', font_family='Arial'))
 			if str(val.minute) == '15' or str(val.minute) == '45':
-				self.dwg.add(self.dwg.line(start=(y, 515), end=(y, 525), stroke_width=2, stroke='black'))
+				self.main_dwg.add(self.main_dwg.line(start=(y, 515), end=(y, 525), stroke_width=2, stroke='black'))
 			if str(val.minute) == '30':
-				self.dwg.add(self.dwg.circle(center=(y, 520), r=4, fill='red'))
+				self.main_dwg.add(self.main_dwg.circle(center=(y, 520), r=4, fill='red'))
 			if str(val.minute) in ('5', '10', '20', '25', '35', '40', '50', '55'):
-				self.dwg.add(self.dwg.line(start=(y, 518), end=(y, 522), stroke_width=1, stroke='black'))
+				self.main_dwg.add(self.main_dwg.line(start=(y, 518), end=(y, 522), stroke_width=1, stroke='black'))
 			y += self.ticks
 			
 	def draw_time_now(self):
@@ -104,32 +102,81 @@ class SVGObject:
 		range_time = int(tm_between.total_seconds() / 60)	
 		y = self.ticks * range_time + 43
 		print(y)
-		self.dwg.add(self.dwg.line(start=(y,0), end=(y, 505), stroke_width=2, stroke='blue', id='time_now'))
+		self.main_dwg.add(self.main_dwg.line(start=(y,0), end=(y, 505), stroke_width=2, stroke='blue', id='time_now'))
 
 	def return_string(self):
 		return self.svg_string
 
-	def __init__(self, filename='test.svg'):
 
-		self.svg_width = 1000
-		self.svg_height = 700
-		self.filename = filename
-		self.ticks = 0
-		self.platforms = self.parse_platforms(SVGObject.JSON)
+	def create_platform_index(self):
+
+		sep_line_height = 2  # Height in pixels for the seperator bar.
+		bottom_border = 200  # Space we need to maintain at the bottom for things like the time bar etc...
+		total_sep = len(self.platforms) - 1  # The number of seperators we need.
+		total_sep_pixels = sep_line_height * total_sep  # How many pixels are taken up by the seperator
+		row_height = ((self.svg_height - bottom_border) - total_sep_pixels) / len(self.platforms)  # The height of each row.
+
+		y = 0
+		alt=True  # Boolean used to alternate row colors.
+
+		# Create definitions
+		index_blue_row = self.index_dwg.rect(id='index_blue_row', size=(self.index_svg_width, row_height), fill='blue')
+		index_green_row = self.index_dwg.rect(id='index_green_row', size=(self.index_svg_width, row_height), fill='green')
+		self.index_dwg.defs.add(index_blue_row)
+		self.index_dwg.defs.add(index_green_row)
 		
+		for platform in self.platforms:
+			
+			#Add background rectangle
+			if alt:
+				#self.main_dwg.add(self.main_dwg.rect((0, y), (self.svg_width, row_height), fill='blue', opacity='0.179'))
+				self.index_dwg.add(self.index_dwg.use(index_blue_row, insert=(0, y)))
+				alt = False
+			else:
+				#self.main_dwg.add(self.main_dwg.rect((0, y), (self.svg_width, row_height), fill='green', opacity='0.179'))
+				self.index_dwg.add(self.index_dwg.use(index_green_row, insert=(0, y)))
+				alt = True
 
-		self.dwg = svgwrite.Drawing(self.filename, (self.svg_width + 30, self.svg_height), profile='tiny')
-		self.platform_row = self.dwg.rect(size=(100,100), id='platform_row_bg', fill='blue', opacity='0.179')
-		self.dwg.defs.add(self.platform_row)
-		self.draw_platform_index()
-		self.start_time, self.end_time = self.parse_times(SVGObject.JSON)
+			y += sep_line_height + row_height
+
+			# Add text label
+			p_text = "Platform {}".format(platform)
+			self.index_dwg.add(self.index_dwg.text(p_text, insert=(10, y - 12), stroke='none', fill='white', font_size='20px', font_weight="bold", font_family="Arial"))
+			
+			# # Add seperator line
+			# if total_sep > 0:  # Ensures we only draw the right amount of seperators
+			# 	self.index_dwg.add(self.index_dwg.line(start=(0,y-1), end=(self.index_svg_width, y-1), stroke_width=2, stroke='black'))
+			# 	total_sep -= 1
+
+
+
+
+	def __init__(self):
+
+		self.main_svg_width = 3000  # Width of the svg layout
+		self.svg_height = 800 # Height of the svg layout.
+		self.index_svg_width = 130 # Width of the platform index column
+
+		self.ticks = 0  # Calcuation of ratio pixels to minutes.
+		self.platforms = self.parse_platforms(SVGObject.JSON)  # Get the platform details from the configuration
+		
+		self.index_dwg = svgwrite.Drawing(id='index_dwg',size=(self.index_svg_width, self.svg_height), profile='tiny')
+		self.main_dwg = svgwrite.Drawing(id='main_dwg', size=(self.main_svg_width + 30, self.svg_height), profile='tiny')
+
+		self.create_platform_index()
+		self.draw_docker_background()
+		
+		# self.platform_row = self.main_dwg.rect(size=(100,100), id='platform_row_bg', fill='blue', opacity='0.179')
+		# self.main_dwg.defs.add(self.platform_row)
+		# self.draw_platform_index()
+		# self.start_time, self.end_time = self.parse_times(SVGObject.JSON)
 		#self.draw_time_line(self.start_time, self.end_time)
-		# self.dwg.add(self.dwg.rect((0,0), (100,100), stroke=svgwrite.rgb(10,10,16,'%'), fill='red'))
+		# self.main_dwg.add(self.main_dwg.rect((0,0), (100,100), stroke=svgwrite.rgb(10,10,16,'%'), fill='red'))
 		#self.draw_time_now()
-		self.dwg.add(self.dwg.use(self.platform_row, insert=(30,30)))
-		self.dwg.add(self.dwg.use(self.platform_row, insert=(50,100), fill='red'))
-		self.dwg.save(pretty=True)
-		self.svg_string = self.dwg.tostring()
+		# self.main_dwg.add(self.main_dwg.use(self.platform_row, insert=(30,30)))
+		# self.main_dwg.add(self.main_dwg.use(self.platform_row, insert=(50,100), fill='red'))
+		#self.main_dwg.save(pretty=True)
+		self.svg_string = '{}{}'.format(self.index_dwg.tostring(), self.main_dwg.tostring())
 
 
 if __name__ == '__main__':
